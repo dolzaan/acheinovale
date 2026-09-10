4ae67fecac26bbe69cc6c35babc4504042bbac46
Paulo Cesar Dolzan <118634208+dolzaan@users.noreply.github.com>
2026-09-10 12:39:21 -0300

    Adicionar fotos turísticas por cidade

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "thumb.wikimedia.org",
+        pathname: "/wikipedia/commons/**",
+      },
+      {
+        protocol: "https",
         hostname: "ftagpobydhyofwsgnjfd.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
